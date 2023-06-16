53016a234f7b3637f9bd5392c3dccd77edf80e0a
TALEX1995 <tanase.alex@libero.it>
2023-06-16 15:55:24 +0200

    Added input and alert

--- script/script.js
+++ script/script.js
@@ -7,13 +7,20 @@ const { createApp } = Vue
 
 // Initialize vue app
 const app = createApp({
-    data () {
+    data: () => {
         return {
+            name: '',
             h1Text: 'Questo testo arriva da js tramite vue',
             immagine: 'https://media.istockphoto.com/id/1201041782/photo/alpaca.jpg?s=612x612&w=0&k=20&c=aHFfLZMuyEyyiJux4OghXfdcc40Oa6L7_cE0D7zvbtY='
         }
+    },
+
+    methods: {
+        nameAlert() {
+            alert(`Il tuo nome è ${this.name}`)
+        }
     }
-})
+});
 
 
 // Insert vue inside HTML
