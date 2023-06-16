2f6da1a28f9a27c9f1abca9c9fe980fe5cf88866
TALEX1995 <tanase.alex@libero.it>
2023-06-16 15:42:40 +0200

    Added h1 and img

--- script/script.js
+++ script/script.js
@@ -10,6 +10,7 @@ const app = createApp({
     data () {
         return {
             h1Text: 'Questo testo arriva da js tramite vue',
+            immagine: 'https://media.istockphoto.com/id/1201041782/photo/alpaca.jpg?s=612x612&w=0&k=20&c=aHFfLZMuyEyyiJux4OghXfdcc40Oa6L7_cE0D7zvbtY='
         }
     }
 })
