266235626ac692fa1343b6b4bd213bce01b0dba4
TALEX1995 <tanase.alex@libero.it>
2023-06-16 15:27:32 +0200

    Added Initialize app and h1

--- script/script.js
+++ script/script.js
@@ -3,3 +3,13 @@ console.log('JS OK', Vue)
 
 // Destructuring createApp
 const { createApp } = Vue
+
+
+// Initialize vue app
+const app = createApp({
+    data () {
+        return {
+            h1Text: 'Questo testo arriva da js tramite vue',
+        }
+    }
+})
